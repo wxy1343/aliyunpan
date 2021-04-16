798ec09fb2f9ad72fc6e3f430d7998653e6418eb
wxy1343 <1343890272@qq.com>
2021-04-17 04:08:51 +0800

    弃用协程 #13

--- aliyunpan/api/req.py
+++ aliyunpan/api/req.py
@@ -1,10 +1,5 @@
-import asyncio
-import functools
-
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
-
-from aliyunpan.api.utils import *
 
 __all__ = ['Req']
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
@@ -29,63 +24,25 @@ class Req:
                 kwargs['headers'].update(self._headers)
             else:
                 kwargs['headers'] = self._headers
-            r = getattr(self._session, method)(*args, **kwargs)
-            logger.debug(r.status_code)
+            r = getattr(self._session, method.lower())(*args, **kwargs)
             return r
         except requests.exceptions.RequestException:
             raise
 
-    async def _req_async(self, method, *args, **kwargs):
-        try:
-            kwargs.setdefault('timeout', self._timeout)
-            kwargs.setdefault('verify', self._verify)
-            kwargs.setdefault('async_timeout', None)
-            async_timeout = kwargs['async_timeout']
-            kwargs.pop('async_timeout')
-            if 'headers' in kwargs:
-                kwargs['headers'].update(self._headers)
-            else:
-                kwargs['headers'] = self._headers
-            return await async_run_sync(functools.partial(getattr(self._session, method), *args, **kwargs),
-                                        async_timeout)
-        except requests.exceptions.RequestException:
-            raise
-        except asyncio.exceptions.TimeoutError:
-            raise
-
-    async def async_run_sync(self, fun, async_timeout):
-        loop = asyncio.get_event_loop()
-        return await asyncio.wait_for(loop.run_in_executor(None, fun), async_timeout)
-
     def get(self, *args, **kwargs):
         return self._req('get', *args, **kwargs)
-
-    async def get_async(self, *args, **kwargs):
-        return await self._req_async('get', *args, **kwargs)
 
     def post(self, *args, **kwargs):
         return self._req('post', *args, **kwargs)
 
-    async def post_async(self, *args, **kwargs):
-        return await self._req_async('post', *args, **kwargs)
-
     def put(self, *args, **kwargs):
         return self._req('put', *args, **kwargs)
-
-    async def put_async(self, *args, **kwargs):
-        return await self._req_async('put', *args, **kwargs)
 
     def head(self, *args, **kwargs):
         return self._req('head', *args, **kwargs)
 
-    async def head_async(self, *args, **kwargs):
-        return await self._req_async('head', *args, **kwargs)
-
     def options(self, *args, **kwargs):
         return self._req('options', *args, **kwargs)
-
-    async def options_async(self, *args, **kwargs):
-        return await self._req_async('options', *args, **kwargs)
 
     def req(self, method, *args, **kwargs):
         return self._req(method, *args, **kwargs)
