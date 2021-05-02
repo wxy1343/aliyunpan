f145a44fb9d9dff000c18ed39a007c119cb0ba64
wxy1343 <1343890272@qq.com>
2021-05-02 20:11:35 +0800

    增加content_hash_name

--- aliyunpan/api/type.py
+++ aliyunpan/api/type.py
@@ -6,4 +6,4 @@ _base_info = ['name', 'id', 'pid', 'type', 'ctime', 'update_time', 'hidden']
 _file_info = (*_base_info, 'category', 'content_type', 'size', 'content_hash_name', 'content_hash', 'download_url')
 FileInfo = namedtuple('FileInfo', _file_info, defaults=('',) * len(_file_info))
 UserInfo = namedtuple('UserInfo', ['id', 'nick_name', 'ctime', 'phone', 'drive_id'], defaults=('',) * 5)
-ShareInfo = namedtuple('ShareInfo', ['name', 'content_hash', 'size', 'path'], defaults=('',) * 4)
+ShareInfo = namedtuple('ShareInfo', ['name', 'content_hash', 'content_hash_name', 'size', 'path'], defaults=('',) * 5)
