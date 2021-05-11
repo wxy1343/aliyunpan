bd3b24917ab4ebbce6a0464e120561bcce2867c9
wxy1343 <1343890272@qq.com>
2021-05-12 04:02:02 +0800

    优化配置文件类

--- aliyunpan/cli/config.py
+++ aliyunpan/cli/config.py
@@ -2,44 +2,59 @@ from pathlib import Path
 
 from ruamel.yaml import YAML
 
+from aliyunpan.common import DATA
+
 __all__ = ['Config']
+
 yaml = YAML()
 
 
 class Config:
     def __init__(self, config_file=None):
-        self._config_file = config_file
+        self._config_file = Path(config_file) if config_file else None
 
     config_file = property(lambda self: self._config_file,
                            lambda self, value: setattr(self, '_config_file', Path(value)))
 
-    def _read(self):
-        with open(self.config_file) as f:
-            return yaml.load(f) or False
+    def read(self):
+        if not self._config_file.is_file():
+            return {}
+        with self.config_file.open(encoding='utf-8') as f:
+            return yaml.load(f) or {}
 
-    def _write(self, conf):
-        with open(self.config_file, 'w') as f:
+    def write(self, conf):
+        if not self._config_file.is_file():
+            if not self._config_file.parent.is_dir():
+                self._config_file.parent.mkdir(parents=True)
+            self._config_file.touch()
+        with self.config_file.open('w', encoding='utf-8') as f:
             yaml.dump(conf, f)
         return True
 
     def get(self, key):
-        conf = self._read()
-        if not conf:
-            raise FileNotFoundError('Configuration file error.')
+        conf = self.read()
         if key in conf.keys():
             value = conf.get(key)
             return value
         return False
 
     def update(self, key, value):
-        conf = self._read() or {}
-        conf.update({key: value})
-        self._write(conf)
+        conf = self.read() or {}
+        if isinstance(value, DATA):
+            value = dict(value)
+        if value is None:
+            del conf[key]
+        else:
+            conf.update({key: value})
+        if conf:
+            self.write(conf)
+        else:
+            self._config_file.unlink()
         return conf
 
     def delete(self, key):
-        conf = self._read() or {}
+        conf = self.read() or {}
         if conf and key in conf:
             conf.pop(key)
-            self._write(conf)
+            self.write(conf)
         return True
